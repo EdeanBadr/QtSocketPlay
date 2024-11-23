d4bcfe04d73c8bb6e715bbf32eaf493c6a98d9a6
Badr Eddine HABIB <habibadro872@gmail.com>
2024-11-23 04:22:28 +0100

    Delete build directory

--- build/CMakeFiles/QTserver.dir/QTserver_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QTserver.dir/QTserver_autogen/mocs_compilation.cpp.o: \
- /home/badr-eddine/QtSocketPlay/build/QTserver_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o: \
- /home/badr-eddine/QtSocketPlay/build/client_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
